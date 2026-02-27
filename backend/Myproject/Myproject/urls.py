c4c1e8a79719ecbd02f7167deb6577da0d3baf80
Abdullatif100 <abdullatifjuma81@gmail.com>
2026-02-27 16:13:59 +0300

    Prepare backend for Render deployment

--- backend/Myproject/Myproject/urls.py
+++ backend/Myproject/Myproject/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 
 
-from django.contrib import admin
-from django.urls import path, include
-from Myproject import settings, views
+from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +29,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
- document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
